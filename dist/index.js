2f7611b01ce4a75cf43c4a017067bb0dde8fe99f
VadimBD <dreamvb5@gmail.com>
2025-11-11 18:07:09 +0200

    Added the first code

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let Name = "Вадим";
+let Age = 21;
+let IsStudent = true;
+console.log(`Добрий день , мене звати ${Name}, мені ${Age} рік.`);
+console.log(`Студент? ${IsStudent ? "Так" : "Ні"}`);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+
+let Name: string = "Вадим";
+let Age:number=21;
+let IsStudent: boolean = true;
+
+console.log(`Добрий день , мене звати ${Name}, мені ${Age} рік.`);
+console.log(`Студент? ${IsStudent ? "Так" : "Ні"}`);
